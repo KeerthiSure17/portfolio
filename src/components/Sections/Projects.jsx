6e774a55c258719462335f07af027681ea5e2e5f
Keerthi Sure <keerthisure1799@gmail.com>
2025-07-20 13:57:11 -0400

    code cleanup

--- src/components/Sections/Projects.jsx
+++ src/components/Sections/Projects.jsx
@@ -42,35 +42,6 @@ const Projects = () => {
                 </a>
               </div>
             </div>
-
-            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2">Portfolio</h3>
-              <p className="text-gray-400 mb-4">
-                Created and deployed a fully responsive portfolio app using
-                React and Tailwind CSS, showcasing projects and skills with
-                dynamic routing and animations.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {projectSkills.map((tech, index) => (
-                  <span
-                    key={index}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <div className="flex justify-between items-center">
-                <a
-                  href="https://keerthisure17.github.io/portfolio"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                >
-                  View Project
-                </a>
-              </div>
-            </div>
           </div>
         </div>
       </RevealOnScroll>
